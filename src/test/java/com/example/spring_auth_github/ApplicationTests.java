bacb196f0b028f97c1fdb3d90df13160aa1d16fa
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-04-08 11:04:18 +0600

    v.1.0.0

---
+++ src/test/java/com/example/spring_auth_github/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_auth_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
